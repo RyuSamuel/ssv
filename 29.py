f569e8c93be89c0415d052e3ab034bf6ea17b186
Sam <rockhead51@naver.com>
2020-12-28 17:18:42 +0900

    12/28

---
+++ 29.py
@@ -0,0 +1,47 @@
+import collections
+
+def numJewelsInStones(J: str, S: str):
+
+    freq = {}
+    counter = 0
+
+    #돌의 빈도 수 합산
+    for char in S:
+        if char not in freq:
+            freq[char] = 1
+        else:
+            freq[char] += 1
+
+    #보석의 빈도 수 합산
+    for char in J:
+        if char in freq:
+            counter += freq[char]
+
+    return counter
+
+print(numJewelsInStones("aA", "aAAbbbb"))
+
+def numJewelsInStonesTwo(J: str, S: str):
+    freq = collections.defaultdict(int)
+    counter = 0
+
+    for char in S:
+        freq[char] += 1
+
+    for char in J:
+        counter += freq[char]
+
+    return counter
+
+print(numJewelsInStonesTwo("aA", "aAAbbbb"))
+
+def numJewlsInStonesThree(J: str, S: str):
+    freq = collections.Counter(S)
+    counter = 0
+
+    for char in J:
+        counter += freq[char]
+
+    return counter
+
+print(numJewlsInStonesThree("aA", "aAAbbbb"))
